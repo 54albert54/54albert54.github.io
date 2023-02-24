eddad52b312791e8f29fe1e2cea79b99005482ec
54albert54 <126289455+54albert54@users.noreply.github.com>
2023-02-24 18:05:58 -0500

    Update sript2.js

--- sript2.js
+++ sript2.js
@@ -471,7 +471,7 @@ class Enemy4M{
 
 
 for (let i = 0 ; i < numberOfEnemies;i++) {
-    enemiesArray.push(new Enemy1M(),new Enemy2()M,new Enemy3()M,new Enemy4()M);
+    enemiesArray.push(new Enemy1M(),new Enemy2()M,new Enemy3()M,new Enemy4M());
     
 }
 

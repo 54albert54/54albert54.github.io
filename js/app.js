d95320c53493a63562c31b25ef5e13037e7551c5
54albert54 <126289455+54albert54@user.noreply.github.com>
2023-08-27 23:59:33 -0400

    ready movil view

--- js/app.js
+++ js/app.js
@@ -1,13 +1,26 @@
+const skillMovil = document.querySelector('.data')
+const infoMovil = document.querySelector('.data-movil')
+const botnSkill = document.querySelector('.botn-skill')
+const botnInfo = document.querySelector('.botn-info')
+const extraInfo = document.querySelector('#extra-info')
+const extraInfoMovile = document.querySelector('.extra-info')
 
-const navBar = document.querySelector('.navbar')
+let newData = '27-agust-2023 v1.1'
+
+extraInfo.innerHTML =  newData
+extraInfoMovile.innerHTML =  newData
 
 
-
-
- 
- const mostrarMenu =()=>{
-  navBar.classList.toggle('aparecer')
- }
- function ocultarMenu(){
-  navBar.classList.add('aparecer')
- }
+const mostrarInfoMovil = ()=>{
+  skillMovil.classList.add('invisible')
+  infoMovil.classList.remove('invisible')
+  botnInfo.style.background = "#fff";
+  botnSkill.style.background = "linear-gradient(#848484,#303030) ";
+}
+const mostrarSkillMovil = ()=>{
+  skillMovil.classList.remove('invisible')
+  infoMovil.classList.add('invisible')
+  botnSkill.style.background = "#000";
+  botnSkill.style.color = "#fff";
+  botnInfo.style.background = "linear-gradient(#848484,#303030) ";
+}

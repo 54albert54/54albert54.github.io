5c15aa90a0a20b9c8e9d5ba29c5b5852adb05be2
54albert54 <126289455+54albert54@user.noreply.github.com>
2023-08-28 00:33:05 -0400

    fixed button side

--- js/app.js
+++ js/app.js
@@ -4,18 +4,26 @@ const botnSkill = document.querySelector('.botn-skill')
 const botnInfo = document.querySelector('.botn-info')
 const extraInfo = document.querySelector('#extra-info')
 const extraInfoMovile = document.querySelector('.extra-info')
+const fixedDiv = document.querySelector('.botnes');
+const originalOffsetTop = fixedDiv.offsetTop;
 
-let newData = '27-agust-2023 v1.1'
+
+let newData = '27-agust-2023 v1.2'
 
 extraInfo.innerHTML =  newData
 extraInfoMovile.innerHTML =  newData
 
+let skillSide = true;
 
+let shadowBLK = '#0d0c0c83';
+let shadowWHT = '#84848453';
+let swadow = shadowWHT
 const mostrarInfoMovil = ()=>{
   skillMovil.classList.add('invisible')
   infoMovil.classList.remove('invisible')
   botnInfo.style.background = "#fff";
   botnSkill.style.background = "linear-gradient(#848484,#303030) ";
+  swadow = shadowBLK
 }
 const mostrarSkillMovil = ()=>{
   skillMovil.classList.remove('invisible')
@@ -23,4 +31,18 @@ const mostrarSkillMovil = ()=>{
   botnSkill.style.background = "#000";
   botnSkill.style.color = "#fff";
   botnInfo.style.background = "linear-gradient(#848484,#303030) ";
+  swadow = shadowWHT
 }
+window.addEventListener('scroll', function () {
+  
+  if (window.pageYOffset >= originalOffsetTop) {
+      fixedDiv.style.position = 'fixed';
+      fixedDiv.style.top = '13px';
+      fixedDiv.style.boxShadow=` 0 4px 20px 4px ${swadow}`;
+      fixedDiv.style.borderRadius='0';
+  } else {
+      fixedDiv.style.position = 'relative';
+      fixedDiv.style.top = '';
+      fixedDiv.style.boxShadow='none';
+  }
+});
